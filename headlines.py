39fdeaed91c19106ea7ddd2bfb2bfd8df21282c5
PCS <prakash.sukhwal@gmail.com>
2017-04-26 13:55:40 +0800

    Now with Jinja templates...git add templates

--- headlines.py
+++ headlines.py
@@ -1,7 +1,7 @@
 # coding=utf8
 #import codecs
 
-from flask import Flask 
+from flask import Flask, render_template
 import feedparser
 
 app = Flask(__name__)
@@ -19,17 +19,9 @@ URLs = {
 @app.route("/<publication>")
 def get_news(publication="bbc"):	
 	feed = feedparser.parse(URLs[publication])
-	first_article = feed["entries"][0]
-	return """<html>
-		<body>
-			<h1> Headlines </h1>
-			<b>{0}</b> <br/>
-			<i>{1}</i> <br/>
-			<p>{2}</p> <br/>
-		</body>
-		</html>""".format(first_article.get("title"), first_article.
-get("published"), first_article.get("summary")).decode('utf-8')
-
+	#first_article = feed["entries"][0]
+	#return render_template("home.html", article=first_article)
+	return render_template("home.html", articles=feed["entries"])
 
 
 if __name__ == "__main__":
